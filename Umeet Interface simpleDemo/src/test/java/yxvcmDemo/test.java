ce0e11005fcd46c1209e0c1102367f904f5ff18a
AdamsYang <284250475@qq.com>
2017-04-27 10:02:46 +0800

    版本更新

--- Umeet Interface simpleDemo/src/test/java/yxvcmDemo/test.java
+++ Umeet Interface simpleDemo/src/test/java/yxvcmDemo/test.java
@@ -1,23 +1,28 @@
 package yxvcmDemo;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.http.client.ClientProtocolException;
 
 import cn.com.umeet.vcm.domain.Meeting;
+import cn.com.umeet.vcm.domain.Terminal;
+import cn.com.umeet.vcm.domain.Video;
 import cn.com.umeet.vcm.session.Session;
 import cn.com.umeet.vcm.session.SessionFactory;
 
 public class test {
 	public static void main(String[] args) throws ClientProtocolException, IOException{
-		Meeting meeting = new Meeting();
-		
-		
-		
-		Session session = new SessionFactory().createSession("admin", "admin", "192.168.22.209:8080");
-		String uuid = session.createConference(meeting);
-		
+			Session session = new SessionFactory().createSession("admin", "admin", "192.168.30.129:8080");
+//			List<Meeting> queryConferenceList = session.queryConferenceList("online");
+//			System.out.println(queryConferenceList.get(0).getName());
+			
+//			List<Terminal> queryTerminal = session.queryTerminal("");
+//			for (Terminal terminal : queryTerminal) {
+//				System.out.println(terminal.getName()+"----->"+terminal.getStatus());
+//			}
 
-		
 	}
 }
